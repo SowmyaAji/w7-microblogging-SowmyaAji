a9d15985557b5b7650d3332c64caf4c0866c42ed
Sowmya <sowmya.aji@gmail.com>
2018-12-16 21:56:28 -0500

    Attempt to load blogs dynamically through JS and API

--- microblogger/api/serializers.py
+++ microblogger/api/serializers.py
@@ -2,6 +2,16 @@ from rest_framework import serializers
 from chilipili.models import Chilipili, User, Follow, Like
 
 
+class CreatableSlugRelatedField(serializers.SlugRelatedField):
+    def to_internal_value(self, data):
+        try:
+            value, _ = self.get_queryset().get_or_create(
+                **{self.slug_field: data})
+            return value
+        except (TypeError, ValueError):
+            self.fail("invalid")
+
+
 class ChilipiliSerializer(serializers.ModelSerializer):
 
     author_user = serializers.SlugRelatedField(
--- microblogger/chilipili/static/js/main.js
+++ microblogger/chilipili/static/js/main.js
@@ -1,3 +1,36 @@
+function chilipiliHtml(chilipili) {
+    return `
+     <div class="chilipili-deets">
+     <h4 class="username">${ chilipili.author_user}</h4>
+     <p class="text">${ chilipili.text}</p> 
+
+    <div class="chilipili-div" data-url="{% url 'like_chilipili' chilipili.pk %}">
+        {% csrf_token %}
+        <button class="btn like-button" ><i class="glyphicon glyphicon-thumbs-up"></i></button>
+        <p>Likes: <span class="like-count">{{ chilipili.likes.count }}</span></p>      
+        
+    </div>
+
+
+    <div class="user-div" data-url="{% url 'follow_user' chilipili.author_user.pk %}">
+        {% csrf_token %}
+        <button class="btn follow-button"><i class="fa fa-angle-double-up">Follow</i></button>
+        <p>Followers:<span class="follow-count">{{ chilipili.author_user.follows.count }}</span></p>
+
+    </div> 
+   </div>     
+    `
+}
+
+$.get('/api/chilipilis/').then(function (chilipili) {
+    console.log('chilipili')
+    for (let chilipili of chilipilis) {
+        $('#chilipili-list').append(chilipiliHtml(chilipili))
+    }
+
+})
+
+
 
 
 $('.like-button').on('click', function (event) {
